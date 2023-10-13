f3f2e916474e70c0bc4243c55da7a492752f52c7
k022g0028@g.neec.ac.jp <k022g0028@g.neec.ac.jp>
2023-10-13 15:30:17 +0900

    企画変更、OBJ置き換え

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    Vector2 dir;
+
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+}
